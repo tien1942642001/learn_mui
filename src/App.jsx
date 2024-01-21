f5ac74b21c95562819c4e648b97ae02086ce9b7d
vu van tien <tien194264@huce.edu.vn>
2024-01-21 22:16:55 +0700

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import FloatingActionButtonSize from "./components/FloatingActionButton";
-import Alert from '@mui/material/Alert';
+import ActionButton from './components/ActionButton';
 
 export default function App() {
   return (
     <>
-      <Alert severity="success">This is a success Alert.</Alert>
-      <Alert severity="info">This is an info Alert.</Alert>
-      <Alert severity="warning">This is a warning Alert.</Alert>
-      <Alert severity="error" hidden="true" onClose={() => {}}>This is an error Alert.</Alert>
-      <FloatingActionButtonSize />
+      <ActionButton />
     </>
   )
 }
